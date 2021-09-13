ea9a1dec6f2c8e4f7be229be3383715999d473e2
Alexander <o.v.drobiazko@gmail.com>
2021-09-13 21:50:01 +0300

    Second commit

---
+++ src/main/java/BaseClass.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseClass {
+    protected WebDriver driver;
+    private final String URL = "https://www.google.com";
+
+    @BeforeTest
+    public void before() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(URL);
+    }
+
+    @AfterTest
+    public void after() {
+        driver.quit();
+    }
+}
---
+++ src/main/java/FirstTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTest extends BaseClass {
+
+    @Test
+    public void firstTest() {
+        WebElement googleElement = driver.findElement(By.xpath("//*[@alt='Google']"));
+        WebElement searchBar = driver.findElement(By.xpath("//input[@class='gLFyf gsfi']"));
+
+
+        Assert.assertTrue(googleElement.isDisplayed(), "The Google page is visible");
+
+        searchBar.sendKeys("Selenium");
+        searchBar.sendKeys(Keys.ENTER);
+        Assert.assertTrue(searchBar.isDisplayed(), "A link is visible");
+
+        searchBar.click();
+        WebElement siteElement = driver.findElement(By.id("td-cover-block-0"));
+        Assert.assertTrue(siteElement.isDisplayed(), "New web site about Selenium is opened");
+    }
+
+
+}
